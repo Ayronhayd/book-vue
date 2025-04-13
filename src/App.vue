36264a1687aa072f120baf8a846646c3fad0dd56
Ayronhayd <DJ_JAZZ2010@mail.ru>
2025-04-13 20:52:15 +0300

    222

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,60 @@
 <template>
- <h2>{{title}}</h2>
- <p>{{ message }}</p>
+
+  <input type="login" v-model="userName" />
+  <br>
+  <input type="password"  v-model="userPassword"/>
+  <br>
+  <input type="email" v-model="userEmail" />
+  <br />
+  <button type="button" @click="userData()">{{ btn }}</button>
 
 
- <button type="button" @click="userData()">{{ btn}}</button>
 
-<div class="wrap">
-<input type="text" v-model="text">
 
-<p>{{ text }}</p>
-</div>
 
+  <div>
+    {{ users }}
+    <button type="button" @click="users = []">Удалить</button>
+  </div>
 </template>
 
 <script>
-
-
 export default {
   data() {
     return {
-      message: 'Hello Vue!',
-      title: 'My First Vue App',
+      users: [],
       btn: 'Отправить',
-      text: ''
+      userName: '',
+      userPassword: '',
+      userEmail: '',
     }
   },
   methods: {
     userData() {
-      this.title= 'New title'
-      this.message= 'New message'
-      this.btn= 'New button'
-
-    }
-  }
+      this.btn = 'Ок'
+      this.users.push({
+        userName: this.userName,
+        userPassword: this.userPassword,
+        userEmail: this.userEmail,
+      })
+    },
+  },
 }
-
 </script>
 
 <style scoped>
 * {
-  margin: 30px;
-  padding: 30px;
+  margin: 0 0 0 10px;
+  padding: 10px;
 }
-
-
 
 h2 {
   color: red;
   font-size: 50px;
-  text-align: center;
-font-family: arial;
 }
 
 p {
   color: green;
   font-size: 30px;
-  text-align: center;
 }
-
 </style>
