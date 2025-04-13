8388b0929c05853e97cfd585ff9c250f585077f3
Ayronhayd <DJ_JAZZ2010@mail.ru>
2025-04-13 20:12:32 +0300

    0.0.0

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+ <h2>{{title}}</h2>
+ <p>{{ message }}</p>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      message: 'Hello Vue!',
+      title: 'My First Vue App'
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+h2 {
+  color: red;
+  font-size: 50px;
+  text-align: center;
+font-family: arial;
+}
+
+p {
+  color: green;
+  font-size: 30px;
+  text-align: center;
+}
+
+</style>
---
+++ src/components/headerNew.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <input type="name">
+  <input type="password">
+  <input type="email">
+  <input type="login">
+  <button type="submit">Отправить</button>
+</div>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+/* Your component styles go here */
+</style>
+
+
