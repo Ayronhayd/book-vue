4cd4b84ed73811242c028a39abd4295e250d86b2
Ayronhayd <DJ_JAZZ2010@mail.ru>
2025-04-14 21:53:15 +0300

    Список ---   v-for

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,18 @@
 <template>
 
-  <input type="login" v-model="userName" />
-  <br>
-  <input type="password"  v-model="userPassword"/>
-  <br>
-  <input type="email" v-model="userEmail" />
-  <br />
-  <button type="button" @click="userData()">{{ btn }}</button>
-
-
-
-
-
-  <div>
-    {{ users }}
-    <button type="button" @click="users = []">Удалить</button>
-  </div>
+<ul>
+  <li v-for="number in numbers" :key="number"> {{ number }} </li>
+</ul>
 </template>
 
 <script>
+
 export default {
   data() {
     return {
-      users: [],
-      btn: 'Отправить',
-      userName: '',
-      userPassword: '',
-      userEmail: '',
+      numbers: [1, 2, 3, 4, 5],
     }
-  },
-  methods: {
-    userData() {
-      this.btn = 'Ок'
-      this.users.push({
-        userName: this.userName,
-        userPassword: this.userPassword,
-        userEmail: this.userEmail,
-      })
-    },
-  },
+  }
 }
 </script>
 
@@ -46,15 +20,5 @@ export default {
 * {
   margin: 0 0 0 10px;
   padding: 10px;
-}
-
-h2 {
-  color: red;
-  font-size: 50px;
-}
-
-p {
-  color: green;
-  font-size: 30px;
 }
 </style>
