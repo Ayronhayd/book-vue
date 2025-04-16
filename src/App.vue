385a3c7717dc3eb2447862484736b9037c325842
Ayronhayd <DJ_JAZZ2010@mail.ru>
2025-04-16 21:24:12 +0300

    0..0...0

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template>
- <div v-show="isVisible">i m visible</div>
- <div>Visibility: {{ isVisible }}</div>
+  <h1> Счетчик {{ counter }}  {{ counter }}</h1>
+
+  <span className="cpap" v-bind:title="massage">Подсказка</span>
 </template>
 
 <script>
 
 export default {
-data() {
-return {
-isVisible: true
+
+  data() {
+  return {
+    counter: 0,
+    massage: 'Hello'
+  }
+  },
+  mounted() {
+    setInterval(() => {
+      this.counter++
+    }, 1000)
+  }
 }
-}}
+
 </script>
 
 <style scoped>
+.cpap {
+  color: red
+}
+
 * {
   margin: 0 0 0 10px;
   padding: 10px;
