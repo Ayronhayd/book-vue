c8a2530a125eb0d10a91e253443f1e9047d885ae
Ayronhayd <DJ_JAZZ2010@mail.ru>
2025-04-24 23:03:29 +0300

    минута 10:40

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,52 @@
 <script setup>
+import { ref } from 'vue';
 
-import ChildComp from './ChildComp.vue'
+const city = ref("Топ 1")
 
-const bt = 'test'
 
 </script>
 
 <template>
-  <ChildComp  :btn="bt" />
+<div className="wrapp">
+  <h1>Погодное приложения {{ city }}</h1>
+  <p>Узнать погоду в вашем городе</p>
+  <input className="text-pass" type="text" placeholder="Введите город">
+  <button className="btn">Узнать</button>
+</div>
 
 
 </template>
+
+<style>
+.wrapp {
+width: 500px;
+height: 300px;
+margin: 0 auto;
+background-color: rgba(211, 214, 13, 0.767);
+border-radius: 12px;
+padding: 20px;
+text-align: center;
+}
+h1 {
+  color: rgb(235, 41, 6);
+}
+
+p {
+color: blueviolet;
+}
+
+.text-pass {
+  padding: 10px;
+  color: rgb(14, 153, 9);
+}
+
+.text-pass::placeholder{
+  color: red;
+}
+
+.btn {
+  padding: 10px;
+  margin-left: 10px;
+}
+
+</style>
---
+++ src/User.vue
@@ -0,0 +1,12 @@
+<template>
+  <div  className="user">
+    <h3>{{ el.name }}</h3>
+    <p>{{ el.email }} - {{ el.pass }}</p>
+  </div>
+</template>
+
+<script setup></script>
+
+
+<style></style>
+
